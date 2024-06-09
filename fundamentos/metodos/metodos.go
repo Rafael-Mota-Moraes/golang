88a1d79f75fa91e0eb326a51f056a37282333e2b
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-09 19:19:00 -0300

    Métodos

---
+++ fundamentos/metodos/metodos.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type usuario struct {
+	nome  string
+	idade uint8
+}
+
+func (u usuario) salvar() {
+	fmt.Printf("Salvando os dados do usuário %s no banco de dados...\n", u.nome)
+}
+
+func (u usuario) maiorDeIdade() bool {
+	return u.idade >= 18
+}
+
+func (u usuario) fazerAniversario() {
+	u.idade++
+}
+
+func main() {
+	usuario1 := usuario{"Usuário 1", 20}
+	fmt.Println(usuario1)
+	usuario1.salvar()
+	usuario2 := usuario{"Davi", 20}
+	usuario2.salvar()
+	maior := usuario2.maiorDeIdade()
+	fmt.Println(maior)
+
+	usuario2.fazerAniversario()
+	fmt.Println(usuario1.idade)
+}
