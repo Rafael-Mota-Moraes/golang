47000e10c4c3843c27f20496454fb884f2288b42
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-09 17:09:20 -0300

    Funções com Ponteiros

---
+++ fundamentos/funcoes-com-ponteiros/funcoes-com-ponteiros.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func inverterSinal(numero int) int {
+	return numero * -1
+}
+
+func inverterSinalComPonteiro(numero *int) {
+	*numero = *numero * -1
+}
+
+func main() {
+	numero := 20
+	numeroInvertido := inverterSinal(numero)
+	fmt.Println(numeroInvertido)
+	inverterSinalComPonteiro(&numero)
+	fmt.Println(numero)
+}
