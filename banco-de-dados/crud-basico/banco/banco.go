40c6835df0df03f4e3a995686e604b5608b8afec
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-14 09:53:03 -0300

    Criando estutura para CRUD

---
+++ banco-de-dados/crud-basico/banco/banco.go
@@ -0,0 +1,24 @@
+package banco
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql" // Driver de conexão com o mysql
+)
+
+// Conectar abre a conexão com o banco de dados
+func Conectar() (*sql.DB, error) {
+	stringConexao := "root:@/cursogolang?charset=utf8&parseTime=true&loc=Local"
+
+	db, erro := sql.Open("mysql", stringConexao)
+
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		return nil, erro
+	}
+
+	return db, nil
+}
