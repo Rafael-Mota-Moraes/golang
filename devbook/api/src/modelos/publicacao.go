95e06ed9d4ecced18dbbe1d23fc547d67c634765
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-20 10:00:16 -0300

    Criação de Publicações

--- devbook/api/src/modelos/publicacao.go
+++ devbook/api/src/modelos/publicacao.go
@@ -6,9 +6,9 @@ import "time"
 type Publicacao struct {
 	ID        uint64    `json:"id,omitempty"`
 	Titulo    string    `json:"titulo,omitempty"`
-	Conteudo  uint64    `json:"conteudo,omitempty"`
+	Conteudo  string    `json:"conteudo,omitempty"`
 	AutorID   uint64    `json:"autorId,omitempty"`
 	AutorNick string    `json:"autorNick,omitempty"`
-	Curtidas  string    `json:"curtidas"`
+	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
 }
