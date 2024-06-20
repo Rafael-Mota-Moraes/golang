017ad97ec64f4eda3685c022c770c3666efe45b1
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-20 09:37:59 -0300

    Rotas de Publicações

---
+++ devbook/api/src/controllers/publicacoes.go
@@ -0,0 +1,18 @@
+package controllers
+
+import "net/http"
+
+// CriarPublicacao cria uma publicação
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {}
+
+// CriarPublicacao busca publicações
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {}
+
+// CriarPublicacao busca uma publicação
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {}
+
+// AtualizarPublicacao atualiza uma publicação
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {}
+
+// DeletarPublicacao deleta uma publicação
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {}
