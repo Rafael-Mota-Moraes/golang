e741812612dbe8bde73432cc148f62f0fffd1c85
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-26 09:51:08 -0300

    Modificando Resposta do Login da API

--- devbook/api/src/controllers/login.go
+++ devbook/api/src/controllers/login.go
@@ -10,6 +10,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
 )
 
 // Login é responsável por autenticar um usuário
@@ -53,5 +54,8 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		respostas.Erro(w, http.StatusInternalServerError, erro)
 	}
 
-	w.Write([]byte(token))
+	usuarioID := strconv.FormatUint(usuarioSalvoNoBanco.ID, 10)
+
+	respostas.JSON(w, http.StatusOK, modelos.DadosAutenticacao{ID: usuarioID, Token: token})
+
 }
