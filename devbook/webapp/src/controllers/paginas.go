879282e9036e88cdae63b64b856e08c66414c82a
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-06-27 09:45:52 -0300

    Renderizando Publicações na Página Principal

--- devbook/webapp/src/controllers/paginas.go
+++ devbook/webapp/src/controllers/paginas.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/modelos"
+	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -20,8 +24,26 @@ func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 // CarregarPagina principal carrega a home do site
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
-	response, erro := http.Get(url)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
+
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeErro(w, response)
+		return
+	}
+
+	var publicacoes []modelos.Publicacao
+	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
 
 	fmt.Println(response.StatusCode, erro)
-	utils.ExecutarTemplate(w, "home.html", nil)
+	utils.ExecutarTemplate(w, "home.html", publicacoes)
 }
